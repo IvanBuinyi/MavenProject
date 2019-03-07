00fa1a010c0e5de7f2cef4fa5476540732119e43
Ivan Buinyi <ivan.buiniy@gmail.com>
2019-03-07 14:34:05 +0200

    MavenCommit2

---
+++ src/test/java/Lesson06/wait_test/TestWait.java
@@ -0,0 +1,73 @@
+package Lesson06.wait_test;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+public class TestWait {
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+
+        driver.get("http://automationpractice.com/index.php");
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.quit();
+    }
+    @Test
+    public void verifyFirstTip(){
+        driver.findElement(By.id("search_query_top")).clear();
+        driver.findElement(By.id("search_query_top")).sendKeys("Dress");
+
+        Assert.assertThat(driver
+                .findElement(By.xpath("//*[@id=\"index\"]/div[2]/ul/li")).getText(),
+                containsString("Dress"));
+
+        driver.findElement(By.id("search_query_top")).clear();
+        driver.findElement(By.id("search_query_top")).sendKeys("T-Shorts");
+
+        Assert.assertThat(driver
+                        .findElement(By.xpath("//*[@id=\"index\"]/div[2]/ul/li[1]")).getText(),
+                containsString("T-Shorts"));
+
+    }
+    void waitForElementDisapearing(By by, int timeout) throws TimeoutException {
+        long initialTime = System.currentTimeMillis();
+        driver.manage().timeouts().implicitlyWait(100,TimeUnit.MILLISECONDS);
+        boolean hp = false;
+        while(true){
+            if (System.currentTimeMillis() - initialTime > timeout){
+                hp=true;
+                break;
+            }
+
+
+            if (driver.findElements(by).isEmpty())
+                break;
+        }
+        driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
+        if(hp){
+            throw new TimeoutException("Element is still present");
+        }
+
+    }
+
+}
